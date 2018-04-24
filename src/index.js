373141be4107eb0a8dbc06e28d9e3b33981c1e85
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-04-23 23:28:00 -0700

    Correctly reset random seed

--- src/index.js
+++ src/index.js
@@ -232,6 +232,9 @@ function addInputHandlers() {
         rng = new Random(s);
         tiles = getRandomTiles(w, h);
         state.initialHouseCount = countHouses(tiles);
+        state.tileHistory = [];
+        state.moveHistory = [];
+        state.swapsMade = 0;
         render(tiles, state);
     }
 
